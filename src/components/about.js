6f7c23d72b26b6eb39dba83fac3875b35901aace
julian so <julianso89@gmail.com>
2018-11-28 15:38:00 -0800

    project set-up complete

---
+++ src/components/about.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default props => (
+    <div>
+        <h1 className="center">Welcome to Our About Us Page</h1>
+        <p>The about component did mount</p>
+    </div>
+);
---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default props => (
+  <div>
+    <h1 className="center">Welcome to Movie Quotes</h1>
+    <p>The home component did mount</p>
+  </div>
+);
