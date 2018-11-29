f407802fe886b1c2a9888ae6a742c7304079d126
julian so <julianso89@gmail.com>
2018-11-28 17:04:46 -0800

    completed fake sign-in sign-out functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,21 @@
-import types from './types';
-import axios from 'axios';
+import types from "./types";
+import axios from "axios";
 
+export function userSignIn() {
+  console.log("User Sign in Called");
+  return {
+    type: types.SIGN_IN
+  };
+}
+
+export function userSignOut() {
+  return {
+    type: types.SIGN_OUT
+  };
+}
+
+export function userSignUp() {
+  return {
+    type: types.SIGN_UP
+  };
+}
